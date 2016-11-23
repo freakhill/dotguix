e8a21b1d6570adebb6f39402a3960cb5425a37f5
johan gall <johan.gall@gmail.com>
2016-11-23 20:53:11 +0900

    moo

---
+++ pearl-metadata/install.sh
@@ -0,0 +1,19 @@
+post_install() {
+    info "NYI"
+}
+
+pre_update() {
+    info "NYI"
+}
+
+post_update() {
+    info "NYI"
+}
+
+pre_remove() {
+    info "NYI"
+}
+
+post_remove() {
+    info "NYI"
+}
